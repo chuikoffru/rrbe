817709a1e0da9281c83a86b74f91cca521ea1f4a
Konstantin Chuykov <chuikoff.ru@gmail.com>
2020-06-09 12:59:53 +1200

    Initial commit

---
+++ src/widgets/Section/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const Section = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+Section.propTypes = {
+
+};
+
+export default Section;
