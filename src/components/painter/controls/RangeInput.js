1031ec75ed9acc688273be640f29d6b369a558cb
Konstantin Chuykov <chuikoff.ru@gmail.com>
2020-06-20 13:20:04 +1200

    Range refactor

--- src/components/painter/controls/RangeInput.js
+++ src/components/painter/controls/RangeInput.js
@@ -1,23 +1,25 @@
 import React, { useCallback } from "react";
 import { Form } from "react-bootstrap";
-import useWidgetSettings from "hooks/useWidgetSettings";
+//import useWidgetSettings from "hooks/useWidgetSettings";
 
-const RangeInput = ({ name, property, options = {} }) => {
+const RangeInput = ({ name, value, onChange, options = {} }) => {
   console.log("Range init");
-  // Получаем настройки конкретного свойства
-  const [range, setRange] = useWidgetSettings(property, 1);
-  // Изменяем значение этого свойства
-  const onChange = useCallback((e) => setRange(e.target.value + "rem"), [
-    setRange,
-  ]);
+
+  const handleChange = useCallback(
+    (e) => {
+      onChange(e.target.value + "rem");
+    },
+    [onChange]
+  );
+
   return (
     <Form.Group>
       <Form.Label>{name}</Form.Label>
       <Form.Control
         {...options}
         type="range"
-        value={parseFloat(range)}
-        onChange={onChange}
+        value={parseFloat(value)}
+        onChange={handleChange}
       />
     </Form.Group>
   );
--- src/components/painter/elements/Appearance.js
+++ src/components/painter/elements/Appearance.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Form } from "react-bootstrap";
 
 import RangeInput from "../controls/RangeInput";
-import ColorPicker from "../controls/ColorPicker";
+//import ColorPicker from "../controls/ColorPicker";
+
+import useWidgetSettings from "hooks/useWidgetSettings";
 
 const Appearance = () => {
   console.log("Appearance init");
 
-  return (
-    <Form>
+  const [fontSize, setFontSize] = useWidgetSettings("styles.fontSize", 1);
+
+  const FontSize = useMemo(
+    () => (
       <RangeInput
         name="Размер текста"
-        property="styles.fontSize"
         options={{ min: 0.2, max: 5, step: 0.1 }}
+        value={fontSize}
+        onChange={setFontSize}
       />
-      <ColorPicker name="Цвет текста" property="styles.color" />
+    ),
+    [fontSize, setFontSize]
+  );
+
+  return (
+    <Form>
+      <FontSize />
+      {/* <ColorPicker name="Цвет текста" property="styles.color" /> */}
     </Form>
   );
 };
