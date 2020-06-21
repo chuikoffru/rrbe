ae50ef2ecd25d7ba0f8fb792fb01908ac17e09a5
Konstantin Chuykov <chuikoff.ru@gmail.com>
2020-06-22 08:24:16 +1200

    Filtered undo redo actions

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,11 +1,18 @@
 import { combineReducers } from "redux";
-import undoable from "redux-undo";
+import undoable, { includeAction } from "redux-undo";
 import { appReducer } from "./app/reducer";
 import { sectionsReducer } from "./sections/reducer";
+import { ADD_SECTION, REMOVE_COLUMNS, ADD_COLUMNS, ADD_WIDGET } from "./types";
 
 export const rootReducer = combineReducers({
   sections: undoable(sectionsReducer, {
     limit: 10,
+    filter: includeAction([
+      ADD_SECTION,
+      REMOVE_COLUMNS,
+      ADD_COLUMNS,
+      ADD_WIDGET,
+    ]),
   }),
   app: appReducer,
 });
