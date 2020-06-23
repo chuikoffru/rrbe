5e4a5504f9783b68eec73a74535907eb1cafcba9
Konstantin Chuykov <chuikoff.ru@gmail.com>
2020-06-24 07:48:08 +1200

    Formatted html result in Export.js

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useRef, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { addSection } from "redux/sections/actions";
@@ -7,10 +7,13 @@ import { ItemTypes } from "helpers/itemTypes";
 
 import DropSectionContainer from "./DropSectionContainer";
 import Sections from "./Sections";
+import Export from "./Export";
 
 const Preview = () => {
   const dispatch = useDispatch();
   const sections = useSelector((state) => state.sections.present.sections);
+
+  const ref = useRef(null);
 
   const addNewSection = useCallback(
     (item) => {
@@ -39,15 +42,24 @@ const Preview = () => {
     [dispatch]
   );
 
+  useEffect(() => {
+    if (sections.length > 0) {
+      console.log("ref.current", ref.current);
+    }
+  }, [sections]);
+
   return (
     <>
-      {sections.map((section, sectionIndex) => (
-        <Sections
-          key={sectionIndex}
-          section={section}
-          sectionIndex={sectionIndex}
-        />
-      ))}
+      <div className="rrbe__preview" ref={ref}>
+        {sections.map((section, sectionIndex) => (
+          <Sections
+            key={sectionIndex}
+            section={section}
+            sectionIndex={sectionIndex}
+          />
+        ))}
+      </div>
+      <Export html={ref.current?.innerHTML} />
       <DropSectionContainer
         accept={[ItemTypes.COMPONENTS, ItemTypes.SECTIONS]}
         onDrop={(item) => addNewSection(item)}
