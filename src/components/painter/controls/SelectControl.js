ae0dba91cec828e0d64a48765901556673041b59
Konstantin Chuykov <chuikoff.ru@gmail.com>
2020-06-25 17:08:04 +1200

    Refactored select control and news

--- src/components/painter/controls/SelectControl.js
+++ src/components/painter/controls/SelectControl.js
@@ -2,14 +2,27 @@ import React, { useCallback } from "react";
 import { Form } from "react-bootstrap";
 
 const SelectControl = ({ name, value, list, onChange, options = {} }) => {
-  console.log("Select init");
-
   const handleChange = useCallback(
     (e) => {
       onChange(e.target.value);
     },
     [onChange]
   );
+
+  const showList = () => {
+    switch (typeof list[0]) {
+      case "object":
+        return Object.entries(list).map(([key, value]) => (
+          <option value={value.value} key={key}>
+            {value.name}
+          </option>
+        ));
+      case "string":
+        return list.map((value, key) => <option key={key}>{value}</option>);
+      default:
+        break;
+    }
+  };
 
   return (
     <Form.Group>
@@ -20,13 +33,7 @@ const SelectControl = ({ name, value, list, onChange, options = {} }) => {
         value={value}
         onChange={handleChange}
       >
-        {typeof list === "object"
-          ? Object.entries(list).map(([key, value]) => (
-              <option value={value.value} key={key}>
-                {value.name}
-              </option>
-            ))
-          : list.map((value, key) => <option key={key}>{value}</option>)}
+        {showList()}
       </Form.Control>
     </Form.Group>
   );
--- src/widgets/News/settings.js
+++ src/widgets/News/settings.js
@@ -1,42 +1,18 @@
-import React, { useCallback } from "react";
-import { Form } from "react-bootstrap";
+import React from "react";
 import useWidgetSettings from "hooks/useWidgetSettings";
-
-export const defaultParams = {
-  widgetName: "News",
-  props: {
-    category: "",
-  },
-};
+import SelectControl from "components/painter/controls/SelectControl";
+import fakeCategories from "./fakeCategories";
 
 const NewsSettings = () => {
-  const { settings, setSettings } = useWidgetSettings();
-
-  const onChange = useCallback(
-    (e) => {
-      setSettings({ ...settings, [e.target.name]: e.target.value });
-    },
-    [setSettings, settings]
-  );
+  const [category, setCategory] = useWidgetSettings("category", "");
 
   return (
-    <Form>
-      <Form.Group>
-        <Form.Label>Категории новостей</Form.Label>
-        <Form.Control
-          as="select"
-          name="category"
-          value={settings.category}
-          onChange={onChange}
-        >
-          <option>Политика</option>
-          <option>Религия</option>
-          <option>Экономика</option>
-          <option>Общество</option>
-          <option>Право</option>
-        </Form.Control>
-      </Form.Group>
-    </Form>
+    <SelectControl
+      name="Категория новостей"
+      value={category}
+      onChange={setCategory}
+      list={fakeCategories}
+    />
   );
 };
 
