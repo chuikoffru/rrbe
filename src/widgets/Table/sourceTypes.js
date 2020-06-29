eb5ad3c01847aeb9754f8bb1099708008de82b85
Konstantin Chuykov <chuikoff.ru@gmail.com>
2020-06-29 17:45:05 +1200

    add render sourceTypes to table widget

--- src/widgets/Table/sourceTypes.js
+++ src/widgets/Table/sourceTypes.js
@@ -1,9 +1,11 @@
 export const sourceTypes = {
   API: "api",
   CUSTOM: "custom",
+  RENDER: "rendering",
 };
 
 export const sourceTypesList = [
   { name: "API", value: sourceTypes.API },
   { name: "В ручную", value: sourceTypes.CUSTOM },
+  { name: "Для рендеринга", value: sourceTypes.RENDER },
 ];
