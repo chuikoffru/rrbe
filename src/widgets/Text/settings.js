dbb9cb8de50d08c1cd8bdf9f37421ae949c76a88
Konstantin Chuykov <chuikoff.ru@gmail.com>
2020-07-01 14:04:16 +1200

    added line-height to text widget

--- src/widgets/Text/settings.js
+++ src/widgets/Text/settings.js
@@ -2,10 +2,15 @@ import React from "react";
 import Wysiwyg from "components/painter/controls/wysiwyg";
 import useWidgetSettings from "hooks/useWidgetSettings";
 import SelectControl from "components/painter/controls/SelectControl";
+import ToggleControl from "components/painter/controls/ToggleControl";
+import RangeControl from "components/painter/controls/RangeControl";
 
 const TextSettings = () => {
   const [wordBreak, setWordBreak] = useWidgetSettings("styles.wordBreak", "normal");
   const [textTransform, setTextTransform] = useWidgetSettings("styles.textTransform", "none");
+  const [fitContent, setFitContent] = useWidgetSettings("styles.width", "100%");
+  const [lineHeight, setLineHeight] = useWidgetSettings("styles.lineHeight", "1.5rem");
+
   return (
     <div className="my-3">
       <Wysiwyg propery="text" />
@@ -26,6 +31,21 @@ const TextSettings = () => {
             name: "Все символы заглавные",
             value: "uppercase",
           },
+        ]}
+      />
+      <RangeControl
+        name="Межстрочный интервал"
+        value={lineHeight}
+        onChange={setLineHeight}
+        options={{ min: 1.0, max: 2.5, step: 0.1, appendix: "rem" }}
+      />
+      <SelectControl
+        name="Ширина текстового контейнера"
+        value={fitContent}
+        onChange={setFitContent}
+        list={[
+          { name: "На всю ширину секции", value: "100%" },
+          { name: "Под ширину контента", value: "fit-content" },
         ]}
       />
       <SelectControl
